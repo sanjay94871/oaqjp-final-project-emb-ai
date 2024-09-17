22c5116a94d6c9ea93b0116796d49300ad630f7b
sanjay94871 <sanjay94871@gmail.com>
2024-09-17 17:57:52 -0400

    Initial Commit

---
+++ server.py
@@ -0,0 +1,25 @@
+
+
+import json
+from flask import Flask,render_template,request
+from EmotionDetection.emotion_detection import emotion_detector
+
+
+app=Flask("project")
+
+@app.route("/emotionDetector")
+def emo_detector():
+    """function to send text to the detector function"""
+    text_to_analyse = request.args.get("textToAnalyze")
+    response = emotion_detector(text_to_analyse)
+    
+    return response
+
+
+@app.route("/")
+def render_index_page():
+    """function to return index html file"""
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5005)
